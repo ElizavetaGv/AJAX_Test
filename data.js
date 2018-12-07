2010bacb3b69ebc73b20e9f7e07fae50036c7997
ElizavetaGv <geand99@yandex.ru>
2018-12-07 19:17:58 +0700

    Версия 1

---
+++ data.js
@@ -0,0 +1,18 @@
+var xhr=new XMLHttpRequest();
+xhr.addEventListener('readystatechange', onReady);
+xhr.open('GET','/data.json',true);
+xhr.send();
+/*показать загрузку*/
+document.getElementById('loader').setAttribute('style', 'display:block;');
+
+function onReady() {
+	if (xhr.readyState==4 && xhr.status==200) {
+		var data=JSON.parse(xhr.responseText);
+		for (var i=0; i<data.length; i++) {
+			var el=document.createElement('div');
+			el.innerText=data[i].name;
+			document.getElementById('list').appendChild(el);			
+		}
+		document.getElementById('loader').setAttribute('style', 'display:none;');
+	}
+}
